4917b1925c3193f4b75d93c217a3cd1b5bf78f74
OliDada <80835762+OliDada@users.noreply.github.com>
2021-04-15 10:51:07 +0000

    upp

--- Verkefni05-Lykkjulist/sketch.js
+++ Verkefni05-Lykkjulist/sketch.js
@@ -1,1 +1,11 @@
 // Hér kemur kóðinn þinn:
+function setup() {
+  createCanvas(500,500);
+  background(255, 215, 0);
+  colorMode(HSB);
+  frameRate(2);
+  strokeWeight(2);
+}
+
+function draw() {
+  
