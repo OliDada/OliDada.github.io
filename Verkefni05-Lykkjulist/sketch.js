39f25290fa88fcb1272ed6b6ebecffae23076a15
OliDada <80835762+OliDada@users.noreply.github.com>
2021-04-15 11:01:24 +0000

    upp

--- Verkefni05-Lykkjulist/sketch.js
+++ Verkefni05-Lykkjulist/sketch.js
@@ -1,4 +1,6 @@
 // Hér kemur kóðinn þinn:
+var i;
+
 function setup() {
   createCanvas(500,500);
   background(255, 215, 0);
@@ -8,4 +10,7 @@ function setup() {
 }
 
 function draw() {
-  
+  for(var x = 10; x < width; x = x + 10){
+  for(var y = 10; y < heigth; y = y + 10){
+    i = int(random(0,10))
+    rect(i,x,y)
