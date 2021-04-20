7764ef35d9e8fae08e5826a42101eef38e5469fa
OliDada <80835762+OliDada@users.noreply.github.com>
2021-04-20 16:10:38 +0000

    test 2

--- Memory-game/sketch.js
+++ Memory-game/sketch.js
@@ -1,48 +1,92 @@
 // Hér kemur kóðinn þinn:
-var x = 0;
-var y = 0;
-var xSpeed = 2;
-var ySpeed = 0;
-var gravity = 0.15;
 
-function setup() {
-	createCanvas(700, 500);
-	noStroke();
-}
+document.addEventListener('DOMContentLoaded', () => {
 
-function draw() {
-  background(255, 200, 0);
-  fill(236,36,94);
-  if(mouseX < width/2 && mouseY < height / 2){
-		rect(0,0,width/2,height/2);
-    fill(100,100,100)
-    ellipse(x, y, 30, 30);
-    x = x + xSpeed;
-    y = y +ySpeed;
-    ySpeed = ySpeed+gravity;
-    if ((x > width/2-15) || (x < 0)) {
-      xSpeed = xSpeed * -1;
-    }
-    if (y > (height/2-15)) {    //til að láta boltann skoppa þegar neðri rönd snertir
-      ySpeed = ySpeed * -0.9;   //skjáinn þarf að draga 15 frá height, því þvermál
-                                //boltans er 30.
+	const cardArray = [
+		{
+			name: 'demantur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'demantur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'ferningur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'ferningur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'hjarta'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'hjarta'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'hringur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'hringur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'sexhyrningur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'sexhyrningur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'stjarna'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'stjarna'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'þríhyrningur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'þríhyrningur'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'blank'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'blank'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'white'
+			img: 'images/demantur.png'
+		},
+		{
+			name: 'white'
+			img: 'images/demantur.png'
+		}
+	]
 
-      y = height/2-15;          //til að boltinn festist ekki
-    }
+	const grid = document.querySelector.('.grid')
+
+	function createBoard() {
+		for (let i = 0; i < cardArray.length; i++) {
+			var card = document.createElement('img')
+			card.setAttribute('src', 'images/blank.png')
+			card.setAttribute('data-id', i)
+			//card.addEventListener('click', flipcard)
+			grid.appendChild(card)
+		}
 	}
 
-  function draw() {
-    if(mouseX > width/2 && mouseY < height/2){
-		  rect(width/2,0,width/2,height/2);
-      fill(100,100,100)
-      if(mouseX > width/2 && mouseY < height/2)
-        ellipse(mouseX, mouseY, 30, 30);
-  }
- }
-	if(mouseX < width/2 && mouseY > height / 2){
-		rect(0,height/2,width/2,height/2);
-	}
-	if(mouseX > width/2 && mouseY > height / 2){
-		rect(width/2,height/2,width/2,height / 2);
-	}
-}
+	createBoard()
