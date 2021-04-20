6980d623e6c76dbd85535b920ea917cb3809a3aa
OliDada <80835762+OliDada@users.noreply.github.com>
2021-04-20 16:37:58 +0000

    Samstæðuspil

--- Memory-game/sketch.js
+++ Memory-game/sketch.js
@@ -77,16 +77,56 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	]
 
+	cardArray.sort(() => 0.5 - Math.random())
+
+
 	const grid = document.querySelector.('.grid')
+	const resultDisplay = document.querySelector('#result')
+	var cardsChosen = []
+	var cardsChosenId = []
+	var cardsWon = []
 
 	function createBoard() {
 		for (let i = 0; i < cardArray.length; i++) {
 			var card = document.createElement('img')
 			card.setAttribute('src', 'images/blank.png')
 			card.setAttribute('data-id', i)
-			//card.addEventListener('click', flipcard)
+			card.addEventListener('click', flipCard)
 			grid.appendChild(card)
 		}
 	}
+
+	function checkForMatch() {
+		var cards = document.querySelectorAll('img')
+		const optionOneId = cardsChosenId(0)
+		const optionTwoId = cardsChosenId(1)
+		if (cardsChosen[0] === cardsChosen[1]) {
+			alert('Samstæða!')
+			cards[optionOneId].setAttribute('src', 'imgages/white.png')
+			cards[optionTwoId].setAttribute('src', 'imgages/white.png')
+			cardsWon.push(cardsChosen)
+		} else  {
+			cards[optionOneId].setAttribute('src', 'images/blank.png')
+		  cards[optionTwoId].setAttribute('src', 'images/blank.png')
+			alert('Reyndu aftur')
+		}
+		cardsChosen = []
+		cardsChosenId = []
+		resultDisplay.textContent = cardsWon.length
+		if (cardsWon.length === cardArray.length/2) {
+			resultDisplay.textContent = 'Þú vannst!'
+	}
+
+
+
+	function flipCard() {
+		var cardId = this.getAttribute('data-id')
+		cardsChosen.push(cardArray[cardId].name)
+		cardsChosenId.push(cardId)
+		this.setAttribute('src', cardArray[cardId].img)
+		if (cardsChosen.length === 2) {
+			setTimeout(checkForMatch, 500)
+	}
+
 
 	createBoard()
