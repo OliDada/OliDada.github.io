6fbba1206f3f33c0a9045e5993bebb59a2f7dba6
OliDada <80835762+OliDada@users.noreply.github.com>
2025-09-29 12:23:42 +0000

    Add files via upload

---
+++ zelda-game/assets/Everything.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Everything" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
+ <image source="Everything.png" width="2048" height="2048"/>
+ <tile id="13980">
+  <animation>
+   <frame tileid="13980" duration="100"/>
+   <frame tileid="13981" duration="100"/>
+   <frame tileid="13982" duration="100"/>
+   <frame tileid="13983" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
